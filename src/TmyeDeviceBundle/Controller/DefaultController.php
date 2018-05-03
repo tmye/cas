b6074a931be3d0ac5c9d862d7cb1210d780a4bb7
abiguime <2597434002@qq.com>
2018-05-03 17:50:37 +0000

    Thu May  3 17:50:37 GMT 2018 _

--- src/TmyeDeviceBundle/Controller/DefaultController.php
+++ src/TmyeDeviceBundle/Controller/DefaultController.php
@@ -3,11 +3,19 @@
 namespace TmyeDeviceBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends Controller
+class DefaultController extends BaseController
 {
+
+    /**
+     * @Route("/server/time",name="serverTime")
+     */
     public function indexAction()
     {
-        return $this->render('TmyeDeviceBundle:Default:index.html.twig');
+
+        $dateTime = new \DateTime();
+        print_r($dateTime);
+        exit;
     }
 }
